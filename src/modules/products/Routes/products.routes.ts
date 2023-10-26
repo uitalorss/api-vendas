359084c4e2c4adfb2b750675b483c6120902c8fc
Uítalo <uitalo.urss@gmail.com>
2023-10-26 11:52:16 -0300

    validação das requisições dos endppoints de produto

--- src/modules/products/Routes/products.routes.ts
+++ src/modules/products/Routes/products.routes.ts
@@ -1,11 +1,57 @@
 import { Router } from 'express';
 import { ProductController } from '../controllers/ProductController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 export const productsRouter = Router();
 const productsController = new ProductController();
 
 productsRouter.get('/', productsController.listProducts);
-productsRouter.get('/:id', productsController.showProduct);
-productsRouter.post('/', productsController.createProduct);
-productsRouter.put('/:id', productsController.updateProduct);
-productsRouter.delete('/:id', productsController.deleteProduct);
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required().messages({
+        'string.guid': 'Informe um valor de identificador válido',
+      }),
+    },
+  }),
+  productsController.showProduct,
+);
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required().messages({}),
+      price: Joi.number().required().messages({}),
+      quantity: Joi.number().required().messages({}),
+    },
+  }),
+  productsController.createProduct,
+);
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required().messages({}),
+      price: Joi.number().required().precision(2).messages({}),
+      quantity: Joi.number().required().messages({}),
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required().messages({
+        'string.guid': 'Informe um valor de identificador válido',
+      }),
+    },
+  }),
+  productsController.updateProduct,
+);
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required().messages({
+        'string.guid': 'Informe um valor de identificador válido',
+      }),
+    },
+  }),
+  productsController.deleteProduct,
+);
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,10 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './routes';
 import { AppError } from '@shared/errors/AppError';
+import { errors } from 'celebrate';
 import '@shared/typeorm';
 
 const app = express();
@@ -11,6 +12,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use(errors());
+
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
