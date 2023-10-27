360386966a8ab0ff649cff057592a5290af58c25
Uítalo <uitalo.urss@gmail.com>
2023-10-27 17:26:46 -0300

    criação da autenticação do usuário ao fazer login

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -2,6 +2,7 @@ import { getCustomRepository } from 'typeorm';
 import { UserRepository } from '../typeorm/repositories/UserRepository';
 import { AppError } from '@shared/errors/AppError';
 import bcrypt from 'bcrypt';
+import jwt, { Secret } from 'jsonwebtoken';
 import { User } from '../typeorm/entities/User';
 
 interface IRequest {
@@ -9,12 +10,21 @@ interface IRequest {
   password: string;
 }
 
+interface IUserResponse {
+  id: string;
+  name: string;
+  email: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
 interface ISession {
-  user: User;
+  user: IUserResponse;
+  token: string;
 }
 
 export class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<ISession> {
     const userRepository = getCustomRepository(UserRepository);
     const user = await userRepository.findOne({
       where: {
@@ -29,6 +39,13 @@ export class CreateSessionService {
       throw new AppError('Usuário e/ou senha inválidos', 404);
     }
 
-    return user;
+    const token = jwt.sign({}, 'd8ad4b5caf5544b6a78887695fe4bbb8', {
+      subject: user.id,
+      expiresIn: '1h',
+    });
+
+    const { password: _, ...allInfoUser } = user;
+
+    return { user: allInfoUser, token };
   }
 }
