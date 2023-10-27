f05cada7019477e4800486431fbc71a82491256b
Uítalo <uitalo.urss@gmail.com>
2023-10-26 21:56:37 -0300

    criação da entidade, migration e repositório do recurso usuário

---
+++ src/modules/users/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,23 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from '../entities/User';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        name: name,
+      },
+    });
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  }
+}
